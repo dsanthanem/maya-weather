f6fe322045a1682e6d8ace0c341991fa7d7e730c
Dinesh Santhanem <dinesh.santhanem@gmail.com>
2017-07-23 17:56:51 +0530

    Announce Time Service to main app(slack-bot-maya)

--- bin/run.js
+++ bin/run.js
@@ -5,9 +5,30 @@ const logger = require('../lib/logger');
 const service = require('../server/service');
 const http = require('http');
 const server = http.createServer(service);
+const request = require('superagent');
 
+server.listen(); // Omitting the port parameter tells NodeJs to choose an available port dynamically
 
 server.on('listening', function () {
+
     logger.info(`MAYA-Time is listening on port ${server.address().port} in ${service.get('env')} mode...`)
-}).listen(3010);
+
+    const announce = () => {
+        request.put(`http://127.0.0.1:3010/service/time/${server.address().port}`, (err, res) => {
+
+            if(err) {
+                logger.info(err);
+                logger.info('Error Connecting to Maya');
+                return;
+            }
+            //logger.info(res.body);
+
+        });
+
+    };
+
+    announce();
+    setInterval(announce, 15*1000);
+
+});
 
